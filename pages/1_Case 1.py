c2cd95f022c1646107904bce289a7367ffa05b34
Jerry <abc@xyz.com>
2024-12-02 10:44:28 +0800

    removed local path to obtain case data and cxr

--- pages/1_Case 1.py
+++ pages/1_Case 1.py
@@ -3,7 +3,7 @@ import streamlit as st
 from components.casePage import CasePage
 from schema.schema import Case
 
-with open('C:\\Users\\Jerry H\\Desktop\\chatbot-app\\data\\cases\\case1.yml', 'r') as file:
+with open('./data/cases/case1.yml', 'r') as file:
     currentCaseYml = yaml.safe_load(file)['case']
 if st.session_state.currentCase != 1:
     st.session_state.currentStage = 1
