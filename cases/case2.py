938b934db5b283dc098d8bde3b77a9b30c3bff19
Jerry <jellybouncejerry@gmail.com>
2024-12-03 09:48:11 +0800

    fixed code and data for case 2

--- cases/case2.py
+++ cases/case2.py
@@ -6,6 +6,6 @@ from schema.schema import Case
 with open('./data/cases/case2.yml', 'r') as file:
     currentCaseYml = yaml.safe_load(file)['case']
 st.session_state.currentStage = 1
-
-currentCase = Case(currentCaseYml['caseNum'], currentCaseYml['caseDesc'], currentCaseYml['questions'])
+currentCase = Case(currentCaseYml['caseNum'], currentCaseYml['caseDesc'],
+                   currentCaseYml['maxStage'], currentCaseYml['questions'])
 currentPage = CasePage(currentCase)
